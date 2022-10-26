8111f8a4d7469ce179f6d5ee5b954ef5dc8641fb
matthew <matthew.ejaz@gmail.com>
2022-10-26 12:43:37 +0200

    avancé profil

--- src/src/Controller/ProfileController.php
+++ src/src/Controller/ProfileController.php
@@ -12,9 +12,9 @@ class ProfileController extends AbstractController
     #[Route('/profile/{pseudo}', name: 'app_profile')]
     public function index($pseudo, UserRepository $userRepo): Response
     {
-        $question = $userRepo->findOneBy(['pseudo'=>$pseudo]);
+        $user = $userRepo->findOneBy(['pseudo'=>$pseudo]);
         return $this->render('profile/index.html.twig', [
-            'profile' => $question,
+            'profile' => $user,
         ]);
     }
 }
