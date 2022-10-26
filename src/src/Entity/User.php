55cbcbe89cad3aca7b315469a9a03880d2cf4aad
Alexis Chochina <alexis@fcinq.com>
2022-10-26 15:08:07 +0200

    fix encore

--- src/src/Entity/User.php
+++ src/src/Entity/User.php
@@ -10,8 +10,6 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
